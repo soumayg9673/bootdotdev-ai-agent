c27c38512e330b4bccc938ca6ed1bf3430de3b5b
soumayg9673 <soumayg9673@zohomail.in>
2026-03-19 19:29:56 +0530

    multiple messages

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
+    # Fetch user prompt command line argument
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=user_prompt
+        contents=messages
     )
-    print("User prompt: ", user_prompt)
+    print("User prompt: ", messages)
     print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
     print("Response tokens: ",response.usage_metadata.candidates_token_count)
     print("Response: ", response.text)
