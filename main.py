fafe7d7001389fc8aeb5d5658051e2f5ad8faa38
soumayg9673 <soumayg9673@zohomail.in>
2026-03-19 19:20:01 +0530

    token usage practice

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=user_prompt
     )
-    print(response.text)
+    print("User prompt: ", user_prompt)
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ",response.usage_metadata.candidates_token_count)
+    print("Response: ", response.text)
+
 
 
 if __name__ == "__main__":
